3d3bfe05fc9adf40e85a55e97f0be3e8de0d6dc7
YellaNikhil <yellanikhil99@gmail.com>
2025-07-09 16:17:17 -0400

    Modified Cmake file to accomadate GLFW submodule and call it from main application.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
 #include<iostream>
 #include<string>
 #include "sdk/cpp/adder.h"
+#include <GLFW/glfw3.h>
+
 int main(){
-    // int count = 1;
-    // std::string name;
-    // while(count < argc){
-    //     name += argv[count++];
-    // }
-    // name.erase(0, name.find_first_not_of(" \t\n\r\f\v"));
-    // name.erase(name.find_last_not_of(" \t\n\r\f\v"));
-    // std::cout << name << std::endl;
+    GLFWwindow* window;
+    /* Init GLFW */
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    window = glfwCreateWindow(400, 400, "Boing (classic Amiga demo)", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
     std::cout << "Hello World\n";
     std::cout << basicmath::add(2, 2) << std::endl;
+    for (;;)
+    {
+
+       /* Swap buffers */
+       glfwSwapBuffers(window);
+       glfwPollEvents();
+
+       /* Check if we are still running */
+       if (glfwWindowShouldClose(window))
+           break;
+    }
+
+    glfwTerminate();
     return 0;
 }
